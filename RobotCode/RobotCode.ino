1aafe6f8a85b96a9396595632932d00038b33777
SimonRockhold <simon@appel-rockhold.org>
2020-02-23 21:55:28 -0800

    Nick's Feb 23 changes

--- RobotCode/RobotCode.ino
+++ RobotCode/RobotCode.ino
@@ -1,17 +1,18 @@
+
 //Download library here https://learn.sparkfun.com/tutorials/tb6612fng-hookup-guide#library-and-example-code, manually import by copying library to User\Documents\Arduino\libraries
 #include <SparkFun_TB6612.h>
 #include "common.h"
 
-Motor motor1 = Motor(AIN1, BIN1, PWMA, offsetA, STBY);
-Motor motor2 = Motor(AIN2, BIN2, PWMB, offsetB, STBY);
+Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY); //I need to check the motor code I wrote for testing to change these. I know offset needs to be changed
+Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Connected");
 
-  //Call calibrate function in setup, comment this out if not needed
-  calibrate();
+  //Call calibrate function in setup, We'll work on that code shortly
+  //calibrate();
 
   //Wait 5 seconds before driving
   delay(5000);
@@ -21,80 +22,51 @@ void setup()
 
 void loop()
 {
-  Serial.println("");
+
   readSensor(); //Collects data from sensors and stores in an array
-  delay(500);
-  //storeData();
-  //printData();
-  //Serial.println(analogRead(IRSensor[0]));
-  //Serial.println("value");
+  IRdirection = convertIR(); //Gets ratio from 0 to 1, going to try and move this higher up to see if maybe it helps?
+
 
   onLine(); //Detects if on line, NEEDS MORE WORK
 
-  if (percentDiff() < TURN_THRESHOLD)
+  delay(500); //Adding this to be able to read/print percent diff values and see if it's a processing speed issue
+  
+  
+  if (tempPercentDiff <= TURN_THRESHOLD)
   { //This function will check to see if there is a significant difference between the left and right sensors, if there is, it will move on to turning prompts, if not it will just drive straight
 
-    goStraight();
+    forward(motor1, motor2, (MAX_SPEED * 0.6));
+    straightForward();
   }
 
   else
   {
-    turning(convertIR()); //starts reading and finds where line is relative to middle
+    propForward(IRdirection); //Changes made in the motor library 
   }
-  //This will store sensor data in a large array
-  storeData(); //changed implementation, see storeData() method
+  //This will store sensor data in a large array, not using rn
+  //storeData();
+  
+  //This will allow it to follow a line for a predetermined amount of time
+  if(millis() >= runTime) {
+    brake(motor1,motor2);
+    blink();
 }
 
-void calibrate()
-{
 
-  //collects sensor data and defines the maximum and minimum line brihtness values
-
-  //delay(5000); //waits 5 seconds to start calibrating
-
-  //initialize temporary max and min, set max equal to real min, min equal to real max
-  int tempMax = SENSOR_MIN;
-  int tempMin = SENSOR_MAX;
-
-  prevTime = millis();
-  while (deltaTime < CALIBRATION_TIME)
-  { // perform calibration for set time.
-    //updateTime();
-    deltaTime = millis() - prevTime;
-
-    readSensor(); //populate sensor data array
-
-    //find max and min over x minutes
-    tempMax = getMax(tempMax);
-    tempMin = getMin(tempMin);
+  //Print things for debugging
+  for(int i = 0; i < NUM_SENSORS; i++) {
+    Serial.print(sensorDataRaw[i]);
+    Serial.print(", ");
   }
 
-  //Now that we have the absolute max and min the sensors found, we can modify those to find an operational range. This will hopefully account for outliers
-  //Modifiers can be increase or decreased as needed
-  //Honestly, we might want to completely remove modifiers, as there's a chance in the IRdirection function it'll create negatives where we don't want them
+  Serial.print("ratio: ");
+  Serial.print(IRdirection);
+  Serial.println(" ");
 
-  maxIR = 0.9 * tempMax;
-  minIR = 1.1 * tempMin;
-}
+  delay(500); //Adding this to let things print/read data
 
-void printData()
-{
-  // prints debug info to serial connection
-  Serial.println("calibrated min and max: ");
-  Serial.println(maxIR);
-  Serial.println(minIR);
-  Serial.println();
-  int readoutIndex = 0;
-  while (readoutIndex < logIndex)
-  {
-    for (int i = 0; i < NUM_SENSORS; i++)
-    {
-      Serial.print(sensorLog[readoutIndex][i]);
-      Serial.print(", ");
     }
-    Serial.println(readoutIndex);
-  }
-}
+
 
 float percentDiff()
 {
@@ -104,46 +76,20 @@ float percentDiff()
   return ((abs((sensorDataRaw[0] + sensorDataRaw[1]) - (sensorDataRaw[3] + sensorDataRaw[4]))) / ((sensorDataRaw[0] + sensorDataRaw[1] + sensorDataRaw[3] + sensorDataRaw[4]) / 2));
 }
 
-// smaller version of getMax and getMin using loops
-int getMax(int tempMax)
-{
-  //Compares all readings from the IR sensors to the temporary max
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
-    tempMax = max(IRSensor[i], tempMax); // sets tempMax to be the larger of the two between each sensor reading and the previous tempMax.
-  }
-  return tempMax; //Returns tempMax to the calibrate while loop
-}
-
-int getMin(int tempMin)
-{
-  //Compares all readings from the IR sensors to the temporary minimum.
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
-    tempMin = min(IRSensor[i], tempMin);
-  }
-  return tempMin; //Returns tempMin to the calibrate while loop
-}
-
-//This should be a better way to find a turning value
-
 //This should be a better way to find a turning value
 float convertIR()
 {
-  int outputArr[NUM_SENSORS]; //Create temporary array to store values
-  int sum; //Initialize sum variable
+  float outputArr[NUM_SENSORS]; //Create temporary array to store values
+  float sum = 0; //Initialize sum variable
+  
   for (int i = 0; i < NUM_SENSORS; i++)
   {
-    //if (i != 2)
-    //{
       outputArr[i] = sensorDataRaw[i] * scaleArray[i]; //stores scaled values
-    //}
   }
 
   for (int i = 0; i < NUM_SENSORS; i++)
   {
-    if (i != 2)
-    {
+    if(i != 2) {
       sum = sum + outputArr[i];
     }
   }
@@ -161,61 +107,180 @@ float convertIR()
     sum = sum - (CENTER_WEIGHT * outputArr[2]);
   }
 
-  //This will constrain to -3*maxIR, 3*maxIR.
-  sum = constrain(sum, -OUTER_WEIGHT * maxIR, OUTER_WEIGHT * maxIR);
+  //This will constrain sum incase somehow it reaches values we wouldn't expect (likely to come up for very sharp turns).
+  //May want to try and remove this
+  sum = constrain(sum, (-OUTER_WEIGHT * maxIR), (OUTER_WEIGHT * maxIR));
 
-  //maps to -1000 to 1000
-  //return map(sum, -MAX_SPEED, MAX_SPEED, -OUTER_WEIGHT * maxIR, OUTER_WEIGHT * maxIR);
-  float temp = map(sum, 0, 1000, -OUTER_WEIGHT * maxIR, OUTER_WEIGHT * maxIR);
-  return temp/1000;
+
+  //maps to 0 to 1, try and mess with this
+  float temp = map(sum, (-OUTER_WEIGHT * maxIR), (OUTER_WEIGHT * maxIR), 0, 1000.00); //Values were in wrong order, they've been rearranged
+
+
+  //Printing for debugging
+  Serial.print("Sum: ");
+  Serial.print(sum);
+  
+  Serial.print(" ratio: ");
+  Serial.print(temp/1000.00);
+
+  Serial.println(" ");
+  
+  return (temp / 1000.00);
 }
 
-//This will make the motors stop in the case that it doesn't detect any line, could later involve turning until it detects something
-//May want to change this to say if all sensors are within ~5%, just incase they read small variations from eachother
+//Continually runs until the sensors detect a line (will be stuck in while loop while all sensors read < minIR*1.1)
 void onLine()
 {
   bool lineDetected = false;
   for (int i = 0; i < NUM_SENSORS; i++)
   {
-    if (sensorDataRaw[i] < minIR)
-    {                      //checks each sensor to see if a line is detected
+    if (sensorDataRaw[i] >= (minIR * 1.3)) //May want to change the 1.3
+    { //checks each sensor to see if a line is detected
       lineDetected = true; //if line is found, set lineDetected flag to true
       break;               // leave the loop, no reason to check other sensors.
     }
   }
-  if (!lineDetected)
+  
+  while (!lineDetected) //changed this notation
   { //if no line was found, brake for set time.
     brake(motor1, motor2);
-    //Code here to stop voltage output to motors, or maybe for testing it could 'shake' the robot
-    delay(0.5 * minutes);
+    blink();
+    readSensor();
+    onLine(); //Brakes motors and keeps blinking while not on the line
   }
 }
 
-//This will take in our number that defines the direction based on which IR sensors are sensing and by how much
-void turning(int IRdirection)
+
+void readSensor()
+{
+  //Serial.println("readSensor()");
+  for (int x = 0; x < NUM_SENSORS; x++)
+{
+    //Serial.println(analogRead(IRSensor[x]));
+    sensorDataRaw[x] = analogRead(IRSensor[x]);
+  }
+}
+
+void blink() //Changed blink to only blink once, but for the online function it calls blink() as long as the device isn't on a line
+  {
+  pinMode(13, OUTPUT);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  }
+
+
+
+
+
+
+
+void updateTime()
+  {
+  //Make sure to always use prevTime = millis() before calling this function, or else it doesn't work
+  deltaTime = prevTime - millis();
+
+}
+
+void propForward(float ratio)
+{
+  motor1.drive(((MAX_SPEED * ratio)), DRIVE_TIME);
+  motor2.drive((MAX_SPEED * (1 - ratio)), DRIVE_TIME);
+  }
+
+//Try this in order to resolve issues around the normal forward() method being blocking
+void straightForward() {
+  motor1.drive((MAX_SPEED * 0.6), DRIVE_TIME);
+  motor2.drive((MAX_SPEED * 0.6), DRIVE_TIME);
+}
+
+
+
+
+
+
+
+
+
+//It would be nice to eventually include a calibrate function, however for now we can simply hard code the IR max and min values based on
+//results we get from our IR sensor only test code
+
+//This is all code for debugging and calibrating, for now it may not be needed
+
+/*void calibrate()
 {
 
-  //We will need to change these based on how the motors are oriented/wired
-  if (IRdirection > 0)
-  {
+  //collects sensor data and defines the maximum and minimum line brihtness values
 
-    motor1.drive(IRdirection, DRIVE_TIME);
-    motor2.drive(MAX_SPEED - IRdirection, DRIVE_TIME);
-  }
-  else if (IRdirection < 0)
-  {
+  //delay(5000); //waits 5 seconds to start calibrating
 
-    motor1.drive(MAX_SPEED + IRdirection, DRIVE_TIME);
-    motor1.drive(-IRdirection, DRIVE_TIME);
+  //initialize temporary max and min, set max equal to real min, min equal to real max
+  int tempMax = SENSOR_MIN;
+  int tempMin = SENSOR_MAX;
+
+  prevTime = millis();
+  while (deltaTime < CALIBRATION_TIME)
+  { // perform calibration for set time.
+    updateTime();
+
+    readSensor(); //populate sensor data array
+
+    //find max and min over x minutes
+    tempMax = getMax(tempMax);
+    tempMin = getMin(tempMin);
   }
+
+  //Now that we have the absolute max and min the sensors found, we can modify those to find an operational range. This will hopefully account for outliers
+  //Modifiers can be increase or decreased as needed
+  //Honestly, we might want to completely remove modifiers, as there's a chance in the IRdirection function it'll create negatives where we don't want them
+
+  maxIR = 0.9 * tempMax;
+  minIR = 1.1 * tempMin;
 }
 
-void goStraight()
+  void printData()
+  {
+  // prints debug info to serial connection
+  Serial.println("calibrated min and max: ");
+  Serial.println(maxIR);
+  Serial.println(minIR);
+  Serial.println();
+  int readoutIndex = 0;
+  while (readoutIndex < logIndex)
 {
-  //This will store the code for going straight, probably just output constant voltage to both motors equally
-
-  forward(motor1, motor2, DRIVE_TIME);
+    for (int i = 0; i < NUM_SENSORS; i++)
+    {
+      Serial.print(sensorLog[readoutIndex][i]);
+      Serial.print(", ");
+    }
+    Serial.println(readoutIndex);
+  }
 }
+
+  
+  
+// smaller version of getMax and getMin using loops
+int getMax(int tempMax)
+{
+  //Compares all readings from the IR sensors to the temporary max
+  for (int i = 0; i < NUM_SENSORS; i++)
+  {
+    tempMax = max(sensorDataRaw[i], tempMax); // sets tempMax to be the larger of the two between each sensor reading and the previous tempMax.
+  }
+  return tempMax; //Returns tempMax to the calibrate while loop
+}
+
+int getMin(int tempMin)
+{
+  //Compares all readings from the IR sensors to the temporary minimum.
+  for (int i = 0; i < NUM_SENSORS; i++)
+  {
+    tempMin = min(sensorDataRaw[i], tempMin);
+  }
+  return tempMin; //Returns tempMin to the calibrate while loop
+}
+
 
 void storeData()
 {
@@ -223,44 +288,10 @@ void storeData()
   if (logIndex < dataPoints)
   { // checks whether or not sensorLog is full.
     for (int i = 0; i < NUM_SENSORS; i++)
-    {
+{
       sensorLog[logIndex][i] = sensorDataRaw[i];
     }
     logIndex++; //increments the log index value.
-  }
 }
-
-void readSensor()
-{
-  //Serial.println("readSensor()");
-  for (int x = 0; x < NUM_SENSORS; x++)
-  {
-    //Serial.println(analogRead(IRSensor[x]));
-    sensorDataRaw[x] = analogRead(IRSensor[x]);
-  }
 }
-
-void blink()
-{
-  pinMode(13, OUTPUT);
-  for (int i = 0; i < 10; i++)
-  {
-    digitalWrite(13, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW);
-  }
-}
-
-void updateTime()
-{
-  deltaTime = prevTime - millis();
-  //if (deltaTime >= duration){
-  // prevTime = millis();
-  //}
-}
-
-void propForward(Motor motor1, Motor motor2, int ratio)
-{
-  motor1.drive(MAX_SPEED * ratio);
-  motor2.drive(MAX_SPEED * (1 - ratio));
-}
+*/
--- RobotCode/common.h
+++ RobotCode/common.h
@@ -2,7 +2,7 @@
 void onLine();
 void goStraight();
 float convertIR();
-void turning(int IRdirection);
+//void turning(int IRdirection);
 void storeData();
 void readSensor();
 float percentDiff();
@@ -12,15 +12,13 @@ void blink();
 void updateTime();
 void setMaxSpeed();
 
-unsigned long minutes = 6000; //This will assign 1 minute in real time to minute variable
+// We weren't using the minute variable
 unsigned long deltaTime; //initialize a change in time variable.
 unsigned long prevTime;
-
+unsigned long runTime = 60000 * 1; //decides how long to run our code, 60,000 is one minute, the second number multiplies that miute
 
 /////////// pin definitions
-const int IRSensor[] = {A2, A1, A0, A7, A6};//Pins reflect current wiring (2/18/2020)
-
-
+const int IRSensor[] = {A2, A1, A0, A7, A6}; //Pins reflect current wiring (2/18/2020)
 
 // to be set when the sensor max and min are known, and put here to improve code flexibility if we change sensors at any point.
 const int NUM_SENSORS = 5;
@@ -32,46 +30,45 @@ const int SENSOR_MIN = 0;
 float maxIR;
 float minIR;
 
-//Initialize IRdirection. This will be a value from -1000 to 1000 that determines our direction, with -1000 is left, 1000 is right, and 0 is forward
+//Initialize IRdirection. This will be a value from 0-1 that determines our direction, with 0 is left, 1 is right, and 0.5 is forward
 float IRdirection;
-
-//Used to limit our max output voltage
-const int MAX_VOLTAGE = 5; //volts
 
 //Initialize array to store sensor values
 int sensorDataRaw[5];
 
-const int CALIBRATION_TIME = 30 * 1000; // sets the calibration time to 30 seconds.
+//const int CALIBRATION_TIME = 30 * 1000; // sets the calibration time to 30 seconds., not using rn
 
-
-const int TURN_THRESHOLD = 0.1; // defines the threshold the deviation must reach before turning.
+const float TURN_THRESHOLD = 0.1; // defines the threshold the deviation must reach before turning.
+float tempPercentDiff;            //placeholder for the percent difference
 
 //These are the multipliers were using to assign weight to inner and outer sensors
 const int INNER_WEIGHT = 2;
 const int OUTER_WEIGHT = 3;
 const float CENTER_WEIGHT = 1; //Dont know if we'll need the center weight, I think I found a workaround
 
-
 // stores the scale factors used in weighted turning.
-const float scaleArray[] = { -OUTER_WEIGHT, -INNER_WEIGHT, CENTER_WEIGHT, INNER_WEIGHT, OUTER_WEIGHT};
+const float scaleArray[] = {-OUTER_WEIGHT, -INNER_WEIGHT, CENTER_WEIGHT, INNER_WEIGHT, OUTER_WEIGHT};
 
+/*
+
+Not using atm
 
 //Initialize variables for data collection
 int logIndex = 0;
 const int dataPoints = 50;
 int sensorLog[dataPoints][NUM_SENSORS]; //rather than store data sequentially,
 //it might be better to use a 2D array to better organize what data points are from the same collection.
-
-
+*/
 
 //                   initialize motor variables
 
-const byte MAX_SPEED = 255;
+const int DRIVE_TIME = 10; //Defines how long (in milliseconds) motors drive in forward functions. Keep this very low to reduce wagging
+const byte MAX_SPEED = 150;
 
 // these constants are used to allow you to make your motor configuration
 // line up with function names like forward.  Value can be 1 or -1
 const int offsetA = 1;
-const int offsetB = 1;
+const int offsetB = -1; //                                                     Change one of these, can't remember which
 
 // Pins for all inputs, keep in mind the PWM defines must be on PWM pins,
 // CHANGE THESE UPON WIRING
@@ -86,4 +83,5 @@ const int offsetB = 1;
 //Optional
 #define STBY 6
 
-const byte DRIVE_TIME = 1; //in milliseconds
+//Not using
+//const byte DRIVE_TIME = 1; //in milliseconds
