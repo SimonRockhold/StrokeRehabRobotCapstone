ae543371100c4dfe5a427219ffd842a5b269dbc3
SimonRockhold <simon@appel-rockhold.org>
2020-02-25 22:48:05 -0800

    Organizational changes: better use of .h files, moved initializations into RobotCode.ino, reorganized Timer into seperate files, and added namespaces to better limit variable scope

---
+++ RobotCode/Timer.h
@@ -0,0 +1,23 @@
+// namespace n
+// {
+class Timer
+{
+private:
+    void updateTime();
+    long interval;
+    bool timeElapsedFlag;
+
+public:
+    unsigned long deltaTime;
+    unsigned long prevTime;
+
+    Timer(long intervalIn)
+    {
+        interval = intervalIn;
+        prevTime = millis();
+        deltaTime = 0;
+        timeElapsedFlag = false;
+    }
+    bool timeElapsed();
+};
+// }
