807773dac6fb880f977bfc3827e37201e10dd5b1
SimonRockhold <simon@appel-rockhold.org>
2020-02-27 17:24:56 -0800

    implemented audioIOTest to play noise on button press, added FileIO, which can read or write a file

--- Experiments/Audio/AudioIOTest.ino
+++ Experiments/Audio/AudioIOTest.ino
@@ -1,20 +1,31 @@
-//#include "commonTest.h"
 #include <arduino.h>
 
-const int beepPin = 5;
-int tones[] = {600, 750, 900};
+const int beepPin = 2;
+const int button = 3;
+int tones[] = {440, 554, 659, 880};
+int length = sizeof(tones)/sizeof(tones[0]);
 
 void setup()
 {
     pinMode(beepPin, OUTPUT);
+    pinMode(button, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 
     //TimerMain();
 }
 
 void loop()
 {
-    chord(50, 4, 100);
-    delay(400);
+    if (digitalRead(button))
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+        chord(200);
+        delay(400);
+    }
+    else
+    {
+        digitalWrite(LED_BUILTIN, LOW);
+    }
 }
 
 void chirp(int freq, int beeps, int duration)
@@ -27,16 +38,14 @@ void chirp(int freq, int beeps, int duration)
         noTone(beepPin);
         delay(100);
     }
-
 }
 
-void chord(int freq, int beeps, int duration)
+void chord(int duration)
 {
-    for (int i = 0; i < beeps; i++)
+    for (int i = 0; i < length; i++)
     {
-        tone(beepPin, (freq/4)*(4+i*2));
+        tone(beepPin, tones[i]);
         delay(duration);
         noTone(beepPin);
-        delay(duration);
     }
 }
---
+++ Experiments/Misc/FileIO.ino
@@ -0,0 +1,58 @@
+
+#include <SD.h>
+#include <SPI.h>
+
+const int cardSelect = 4;
+
+void setup()
+{
+    pinMode(13, INPUT);
+    File testFile;
+    // Open serial communications and wait for port to open:
+    Serial.begin(9600);
+    while (!Serial)
+    {
+        ; // wait for serial port to connect. Needed for native USB port only
+    }
+
+    Serial.print("Initializing SD card...");
+
+    if (!SD.begin(cardSelect))
+    {
+        Serial.println("initialization failed!");
+        while (1)
+            ;
+    }
+    Serial.println("initialization done.");
+
+    if (SD.exists("test.txt"))
+    {
+        Serial.println("example.txt exists. opening file");
+        testFile = SD.open("test.txt", FILE_READ);
+    }
+    else
+    {
+        Serial.println("example.txt doesn't exist.");
+    }
+
+    digitalWrite(13, testFile);
+
+    if (testFile)
+    {
+        while (testFile.available())
+        {
+            Serial.write(testFile.read());
+        }
+        testFile.close(); //testFile.println("test test, one two three");
+    }
+    else
+    {
+        Serial.println("write failed");
+        while (true)
+            ;
+    }
+}
+
+void loop()
+{
+}
